031e5a316bfa8c6ba8b22cf8032c89cf84e08728
mguilmineau714 <mguilmineau714@student.fuhsd.org>
2023-09-26 13:29:55 -0700

    Almost done w/ in class lab

--- P3_Guilmineau_Marcely_Math.java
+++ P3_Guilmineau_Marcely_Math.java
@@ -1,28 +1,29 @@
-import java.lang.Math.*;
+import java.awt.Point;
 
-public class P3_Guilmineau_Marcely_Math {
-    static double cToF(double c) {
+public class P3_Guilmineau_Marcely_Math {    
+     static double cToF(double c) {
         return c*1.8+32;
     }
     static double fToC(double f) {
-        return (5/9)*(f-32);
+        return (5.0/9.0)*(f-32.0);
     }
     static double sphereVolume(double r) {
-        return (4/3)*Math.PI*Math.pow(r, 3);
+        return (4.0/3.0)*Math.PI*Math.pow(r, 3);
     }
     static double hypoteneuse(double side1, double side2) {
         return Math.pow(Math.pow(side1, 2) + Math.pow(side2, 2), 0.5);
     }
     static double gramsToAtoms(double grams, double atomicMass) {
-        return grams/atomicMass;
+        final double AVOGADROS_CONSTANT = 6.022140857 * Math.pow(10, 23);
+        return grams/atomicMass * AVOGADROS_CONSTANT;
     }
     static int randomN(int min, int max) {
         return min + (int)(Math.random() * ((max - min) + 1));
     }
-    static double perimeter(double a, double b, double c) {
-        double abdist = Math.pow(Math.pow(a, 2) + Math.pow(b, 2), 0.5);
-        double acdist = Math.pow(Math.pow(a, 2) + Math.pow(c, 2), 0.5);
-        double bcdist = Math.pow(Math.pow(b, 2) + Math.pow(c, 2), 0.5);
+    static double perimeter(Point a, Point b, Point c) {
+        double abdist = Point.distance(a.getX(), a.getY(), b.getX(), b.getY());
+        double acdist = Point.distance(b.getX(), b.getY(), c.getX(), c.getY());
+        double bcdist = Point.distance(a.getX(), a.getY(), c.getX(), c.getY());
         return abdist + acdist + bcdist;
     }
 }
---
+++ P3_Guilmineau_Marcely_MathDriver.java
@@ -0,0 +1,57 @@
+import java.awt.Point;
+
+public class P3_Guilmineau_Marcely_MathDriver {
+    public static void main(String[] args) {
+        // Temperatures
+        double temp1 = 212;
+        double temp2 = 98.6;
+        double temp3 = 37;
+        double temp4 = -15;
+        System.out.println(temp1 + "\u00b0" + "C" + " --> " + P3_Guilmineau_Marcely_Math.fToC(temp1) + "\u00b0" + "C");
+        System.out.println(temp2 + "\u00b0" + "C" + " --> " + P3_Guilmineau_Marcely_Math.fToC(temp2) + "\u00b0" + "C");
+        System.out.println(temp3 + "\u00b0" + "F" + " --> " + P3_Guilmineau_Marcely_Math.cToF(temp3) + "\u00b0" + "F");
+        System.out.println(temp4 + "\u00b0" + "F" + " --> " + P3_Guilmineau_Marcely_Math.cToF(temp4) + "\u00b0" + "F");
+
+        // Spheres
+        double rad1 = 1.0;
+        double rad2 = 6.8;
+        System.out.println("Volume of a sphere with radius "  + rad1 + " is " + P3_Guilmineau_Marcely_Math.sphereVolume(rad1));
+        System.out.println("Volume of a sphere with radius "  + rad2 + " is " + P3_Guilmineau_Marcely_Math.sphereVolume(rad2));
+        
+        // Triangles
+        double side1 = 3;
+        double side2 = 4;
+        double side3 = 2.5;
+        double side4 = 9.25;
+        System.out.println("A right triangle with sides " + side1 + " and " + side2 + " has hypotenuse " + P3_Guilmineau_Marcely_Math.hypoteneuse(side1, side2));
+        System.out.println("A right triangle with sides " + side3 + " and " + side4 + " has hypotenuse " + P3_Guilmineau_Marcely_Math.hypoteneuse(side3, side4));
+        
+        // Atoms
+        double feGrams = 0.75;
+        double auGrams = 5.24;
+        double neGrams = 2.0;
+        double feMass = 55.845;
+        double auMass = 196.96657;
+        double neMass = 20.1797;
+        System.out.println(feGrams + "g of Fe contains " + P3_Guilmineau_Marcely_Math.gramsToAtoms(feGrams, feMass) + " atoms");
+        System.out.println(auGrams + "g of Au contains " + P3_Guilmineau_Marcely_Math.gramsToAtoms(auGrams, auMass) + " atoms");
+        System.out.println(neGrams + "g of Ne contains " + P3_Guilmineau_Marcely_Math.gramsToAtoms(neGrams, neMass) + " atoms");
+    
+        // Randoms
+        int randA = 7;
+        int randB = 9;
+        for(int i = 0; i < 6; i++) {
+            System.out.println("A random number between " + randA + " and " + randB + " is: " + P3_Guilmineau_Marcely_Math.randomN(randA, randB));
+        }
+        
+        // Perimeters
+        Point corner1 = new Point(1, 2);
+        Point corner2 = new Point(3, 4);
+        Point corner3 = new Point(5, 1);
+        System.out.println("A triangle with vertices:");
+        System.out.println("\t" + corner1.toString());
+        System.out.println("\t" + corner2.toString());
+        System.out.println("\t" + corner3.toString());
+        System.out.println("has perimeter " + P3_Guilmineau_Marcely_Math.perimeter(corner1, corner2, corner3));
+    }
+}
