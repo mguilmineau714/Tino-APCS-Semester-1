ee0582a698d321749cb4116e2bbce0b39500172f
mguilmineau714 <129220610+mguilmineau714@users.noreply.github.com>
2023-10-03 18:28:54 -0700

    finished koch curve lab

--- P3_Guilmineau_Marcely_KochCurve.java
+++ P3_Guilmineau_Marcely_KochCurve.java
@@ -1,25 +1,69 @@
 import gpdraw.*;
+/*
+    Name:       Marcely Guilmineau
+    Date:       10/3/2023
+    Period:     3
 
-public class P3_Guilmineau_Marcely_KochCurve{
-    public static void main(String[] args) {
-        SketchPad paper = new SketchPad(1000, 1000);
-        DrawingTool pen = new DrawingTool(paper);
-        P3_Guilmineau_Marcely_KochCurve curvy = new P3_Guilmineau_Marcely_KochCurve();
-        curvy.drawKochCurve(6, 3000, pen);
+    Is this lab fully working?  (Yes)
+    If not, explain:
+    If resubmitting, explain:
+*/
+public class P3_Guilmineau_Marcely_KochCurve {
+    static SketchPad paper;
+    static DrawingTool pen;
+    
+    public P3_Guilmineau_Marcely_KochCurve(int worldWidth, int worldHeight)  {
+        paper = new SketchPad(worldWidth, worldHeight);
+        pen = new DrawingTool(paper);
     }
     
-    public void drawKochCurve(int level, int length, DrawingTool pen) {
+    public void drawKochSnowflake2(int level, int length) {
+        drawKochCurve2(level, length);
+        pen.turnRight(90);
+        drawKochCurve(level, length+200);
+        pen.turnRight(90);
+        drawKochCurve2(level, length);
+        pen.turnRight(90);
+        drawKochCurve(level, length+200);
+    }
+    
+    public void drawKochSnowflake(int level, int length) {
+        pen.turnRight(30);
+        drawKochCurve(level, length);
+        pen.turnRight(120);
+        drawKochCurve(level, length);
+        pen.turnRight(120);
+        drawKochCurve(level, length);
+    }
+    
+    public void drawKochCurve(int level, int length) {
         if(level == 1) {    
             pen.down();
             pen.move(length);
         } else {
-            drawKochCurve(level-1, length/3, pen);
+            drawKochCurve(level-1, length/3);
             pen.turnLeft(60);
-            drawKochCurve(level-1, length/3, pen);
+            drawKochCurve(level-1, length/3);
             pen.turnRight(120);
-            drawKochCurve(level-1, length/3, pen);
+            drawKochCurve(level-1, length/3);
             pen.turnLeft(60);
-            drawKochCurve(level-1, length/3, pen);
+            drawKochCurve(level-1, length/3);
         }
     }
+    
+    public void drawKochCurve2(int level, int length) {
+        if(level == 1) {    
+            pen.down();
+            pen.move(length);
+        } else {
+            drawKochCurve2(level-1, length/3);
+            pen.turnLeft(-60);
+            drawKochCurve2(level-1, length/3);
+            pen.turnRight(-120);
+            drawKochCurve2(level-1, length/3);
+            pen.turnLeft(-60);
+            drawKochCurve2(level-1, length/3);
+        }
+    }
+    
 }
---
+++ P3_Guilmineau_Marcely_KochCurveDriver.java
@@ -0,0 +1,16 @@
+import gpdraw.*;
+/*
+    Name:       Marcely Guilmineau
+    Date:       10/1/2023
+    Period:     3
+
+    Is this lab fully working?  (Yes)
+    If not, explain:
+    If resubmitting, explain:
+*/
+public class P3_Guilmineau_Marcely_KochCurveDriver {
+   public static void main(String[] args) {
+       P3_Guilmineau_Marcely_KochCurve koch = new P3_Guilmineau_Marcely_KochCurve(1000, 1000);
+       koch.drawKochSnowflake(6, 300);
+   }
+}
