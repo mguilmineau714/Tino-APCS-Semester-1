255af98edae03996ea4cb44de8650a19d4a6261d
mguilmineau714 <mguilmineau714@student.fuhsd.org>
2023-10-18 11:24:20 -0700

    added gradient option to myfractal

--- P3_Guilmineau_Marcely_MyFractal.java
+++ P3_Guilmineau_Marcely_MyFractal.java
@@ -1,14 +1,11 @@
 import gpdraw.*;
+import java.awt.Color;
 /*
     Name:       Marcely Guilmineau
     Date:       10/16/2023
     Period:     3
     Fractal Name: Customizable Pentigree Star
     This class draws a fractal of a star made out of numCorners pentigrees.
-    Note: 
-    If 2 methods are not allowed, then please only consider my pentigree method.
-    I assumed they're allowed since koch snowflake also required 2 methods.
-
     Is this lab fully working?  (Yes)
     If not, explain:
     If resubmitting, explain:
@@ -21,33 +18,58 @@ public class P3_Guilmineau_Marcely_MyFractal {
     public static void main(String[] args) {
         paper = new SketchPad(2000, 2000);
         pen = new DrawingTool(paper);
-        drawPentigreeStar(5, 200, 5);
+        int level = 0;
+        double length = 200;
+        int numCorners = 6;
+        Color starColor = new Color(0, 0, 255);
+        Boolean makeGradient = true;
+        pen.up();
+        pen.move(-200, 300);
+        pen.down();
+        drawPentigreeStar(0, length, numCorners, starColor, makeGradient);
+        pen.up();
+        pen.move(200, 300);
+        pen.down();
+        drawPentigreeStar(1, length, numCorners, starColor, makeGradient);
+       // drawPentigreeStar(2, length, numCorners, starColor, makeGradient);
+       // drawPentigreeStar(3, length, numCorners, starColor, makeGradient);
     }
     
-    public static void drawPentigreeStar(int level, double length, int numCorners) {
+    public static void drawPentigreeStar(int level, double length, int numCorners, Color starColor, Boolean makeGradient) {
         double angle = 360.0/numCorners;
+        int R = starColor.getRed();
+        int G = starColor.getGreen();
+        int B = starColor.getBlue();
+        Color startColor;
         for(int i = 0; i < numCorners; i++) {
-            drawPentigree(level, length);
+            startColor = new Color(R, G, B);
+            if(makeGradient) {
+                R += R >= 255-255/numCorners ? 255-R : 255/numCorners;
+                G += G >= 255-255/numCorners ? 255-G : 255/numCorners;
+                B += B >= 255-255/numCorners ? 255-B : 255/numCorners;
+            }
+            drawPentigree(level, length, startColor);
             pen.turnLeft(angle);
         }
     }
     
-     public static void drawPentigree(int level, double length) {
-        if(level == 0) {
+     public static void drawPentigree(int level, double length, Color color) {
+        pen.setColor(color);
+         if(level == 0) {
             pen.move(length);
         } else {
             pen.turnLeft(36);
-            drawPentigree(level-1, length*SCALE);
+            drawPentigree(level-1, length*SCALE, color);
             pen.turnLeft(72);
-            drawPentigree(level-1, length*SCALE);
+            drawPentigree(level-1, length*SCALE, color);
             pen.turnRight(144);
-            drawPentigree(level-1, length*SCALE);
+            drawPentigree(level-1, length*SCALE, color);
             pen.turnRight(72);
-            drawPentigree(level-1, length*SCALE);
+            drawPentigree(level-1, length*SCALE, color);
             pen.turnLeft(72);
-            drawPentigree(level-1, length*SCALE);
+            drawPentigree(level-1, length*SCALE, color);
             pen.turnLeft(72);
-            drawPentigree(level-1, length*SCALE);
+            drawPentigree(level-1, length*SCALE, color);
             pen.turnRight(36);
         }
     }
