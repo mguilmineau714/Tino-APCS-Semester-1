452bc9702384261d7bdbab249ede4d077b5f9640
mguilmineau714 <129220610+mguilmineau714@users.noreply.github.com>
2023-10-17 23:07:26 -0700

    finished myfractal and palindrome labs

--- P3_Guilmineau_Marcely_MyFractal.java
+++ P3_Guilmineau_Marcely_MyFractal.java
@@ -1,8 +1,13 @@
 import gpdraw.*;
 /*
     Name:       Marcely Guilmineau
-    Date:       10/7/2023
+    Date:       10/16/2023
     Period:     3
+    Fractal Name: Customizable Pentigree Star
+    This class draws a fractal of a star made out of numCorners pentigrees.
+    Note: 
+    If 2 methods are not allowed, then please only consider my pentigree method.
+    I assumed they're allowed since koch snowflake also required 2 methods.
 
     Is this lab fully working?  (Yes)
     If not, explain:
@@ -10,21 +15,40 @@ import gpdraw.*;
 */
 public class P3_Guilmineau_Marcely_MyFractal {
     static SketchPad paper;
-    static DrawingTool p;
+    static DrawingTool pen;
+    static final double SCALE = (3-Math.pow(5, 0.5))/2;
+    
     public static void main(String[] args) {
         paper = new SketchPad(2000, 2000);
-        p = new DrawingTool(paper);
-        P3_Guilmineau_Marcely_MyFractal fractal = new P3_Guilmineau_Marcely_MyFractal();
-        fractal.drawPentagon(50, 0, 0);
-        fractal.drawSierpinskiPentagon(0, 100);
+        pen = new DrawingTool(paper);
+        drawPentigreeStar(5, 200, 5);
     }
     
-    public void drawSierpinskiPentagon(int level, double sideLength) {
-        
+    public static void drawPentigreeStar(int level, double length, int numCorners) {
+        double angle = 360.0/numCorners;
+        for(int i = 0; i < numCorners; i++) {
+            drawPentigree(level, length);
+            pen.turnLeft(angle);
+        }
     }
     
-    public void drawPentagon(int sideLength, int centerX, int centerY) {
-        p.up();
+     public static void drawPentigree(int level, double length) {
+        if(level == 0) {
+            pen.move(length);
+        } else {
+            pen.turnLeft(36);
+            drawPentigree(level-1, length*SCALE);
+            pen.turnLeft(72);
+            drawPentigree(level-1, length*SCALE);
+            pen.turnRight(144);
+            drawPentigree(level-1, length*SCALE);
+            pen.turnRight(72);
+            drawPentigree(level-1, length*SCALE);
+            pen.turnLeft(72);
+            drawPentigree(level-1, length*SCALE);
+            pen.turnLeft(72);
+            drawPentigree(level-1, length*SCALE);
+            pen.turnRight(36);
+        }
     }
-    
 }
--- P3_Guilmineau_Marcely_RecursiveReverseAndPalindrome.java
+++ P3_Guilmineau_Marcely_RecursiveReverseAndPalindrome.java
@@ -8,24 +8,29 @@
     If resubmitting, explain:
 */
 public class P3_Guilmineau_Marcely_RecursiveReverseAndPalindrome {
-    
     public static void main(String[] args) {
-        /*
+        // Test recursiveStringReverse
         System.out.println(recursiveStringReverse("123456789"));
         System.out.println(recursiveStringReverse("12345678"));
         System.out.println(recursiveStringReverse("a"));
-        System.out.println(recursiveStringReverse("fake empty string"));
-        System.out.println(recursiveStringReverse(""));*/
+        System.out.println(recursiveStringReverse(""));
         
-        System.out.println(recursiveIsPalindrome("gaga"));
+        //Test recursiveIsPalindrome
+        System.out.println(recursiveIsPalindrome("radar"));
+        System.out.println(recursiveIsPalindrome("J"));
+        System.out.println(recursiveIsPalindrome(" Lewd did I live, & evil I did dwel."));
+        System.out.println(recursiveIsPalindrome("I like Java"));
+        System.out.println(recursiveIsPalindrome(" Straw? No, too stupid a fad, I put soot on warts."));
+        System.out.println(recursiveIsPalindrome("***Nurse!*** I spy gypsies....run!!!!!  "));
+        System.out.println(recursiveIsPalindrome(""));
     }
     
     public static String recursiveStringReverse(String str) {
         return str.length() < 2 ? str : str.substring(str.length()-1) + recursiveStringReverse(str.substring(0, str.length()-1));
     }
     
-    public static Boolean recursiveIsPalindrome(String str) {
-        String newString = str.replaceAll("\\p{P}", "").toUpperCase();
-        return newString.substring(0, str.length()/2).equals(newString.substring(str.length()/2));
+    public static boolean recursiveIsPalindrome(String s) {
+      String str = s.replaceAll("[^a-zA-Z]", "").toUpperCase();
+      return str.length() < 2 || str.charAt(0) == str.charAt(str.length()-1) && recursiveIsPalindrome(str.substring(1, str.length()-1));
     }
 }
