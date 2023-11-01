af3340d86c6388e8ebacd9f1a5732489208e0487
mguilmineau714 <129220610+mguilmineau714@users.noreply.github.com>
2023-10-31 23:41:55 -0700

    finished shorthand

--- P3_Guilmineau_Marcely_Shorthand.java
+++ P3_Guilmineau_Marcely_Shorthand.java
@@ -1,4 +1,3 @@
-
 /*
     Name:       Marcely Guilmineau
     Date:       10/31/2023
@@ -10,26 +9,58 @@
 */ 
 
 public class P3_Guilmineau_Marcely_Shorthand {
+    
      public static void main(String[] args) {
-         System.out.print(translateToShortHand("Me AND YOU forever!"));
+         System.out.println(translateToShortHand("I LOvE APCS! i WilL get a FiVe on the ap teSt :)"));
+         System.out.println(translateToShortHand("A truck"));
+         System.out.println(translateToShortHand("Me AND YOU forever!"));
+         System.out.println(translateToShortHand("To Bill and Ted: Are you going to be EXCELLENT to towtrucks too???"));
+         System.out.println(translateToShortHand("Look, here's a 3.14159 I made for you! And? How's it taste?"));
+         System.out.println(translateToShortHand("MY mAmA aLWaYs said, \"LIFe iS like a box of chocolates...you never know what you're gonna get!\""));
+         System.out.println(translateToShortHand("!!aNd!! !tO? ****yOu***foR** fork toward young fo"));
      }
      
      public static String translateToShortHand(String str) {
         String s = "";
         for(int i = 0; i < str.length(); i++) {
-            checkStringAndAdd(str, s, "and", "&", i);
-            s += str.charAt(i);
+            if(getWord(str, i).equalsIgnoreCase("and")) {
+                s += "&";
+                i += getWord(str, i).length();
+            }
+            if(getWord(str, i).equalsIgnoreCase("to")) {
+                s += "2";
+                i += getWord(str, i).length();
+            }
+            if(getWord(str, i).equalsIgnoreCase("you")) {
+                s += "u";
+                i += getWord(str, i).length();
+            }
+            if(getWord(str, i).equalsIgnoreCase("for")) {
+                s += "4";
+                i += getWord(str, i).length();
+            }
+            char ch = Character.toLowerCase(str.charAt(i));
+            if((ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u') || !(i != 0 && Character.isLetter(str.charAt(i-1)) || (i != str.length()-1 && Character.isLetter(str.charAt(i+1))))) {
+                s += str.charAt(i);
+            }
         }
         return s;
      }
      
-     public static void checkStringAndAdd(String checkString, String addString, String lookString, String replaceString, int i) {
-          if(checkString.substring(i, i+addString.length()-1).equals(lookString)) {
-                if(i == 0 || i+addString.length() == checkString.length() || (!Character.isLetter(checkString.charAt(i-1)) && !Character.isLetter(checkString.charAt(i+4)))) {
-                    addString += replaceString;
-                    i += addString.length();
-                }
-         }
-     }
+     public static String getWord(String str, int startIndex) {
+      String a = str.substring(startIndex);
+      String b = "";
+      for(int i = 0; i < a.length(); i++) {
+        if(Character.isLetter(a.charAt(i))) {
+          b += a.charAt(i);
+        } else {
+          break;
+        }
+      }
+      return b;
+    }
+
 }
+
+
 
